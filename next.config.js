cbe0f2add4d7ee99e25b39b2b00b6b0362fb2b27
Varsha Kodi <varsha.kodi4@gmail.com>
2026-08-28 05:05:47 +0400

    Stop exposing MONGODB_URI and JWT_SECRET to the client bundle

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'images.unsplash.com'],
   },
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  }
 }
 
 module.exports = nextConfig
